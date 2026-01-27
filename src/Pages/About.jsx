a05f2cdf6d23cffb28a53921977eb8dca7fd9a8b
vibol18 <vibol18@gmail.com>
2026-01-27 08:53:24 +0700

    update

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
+import { Typewriter } from 'react-simple-typewriter'
 
 function About() {
   return (
     <div>
       <section id='about' className='max-w-6xl mx-auto p-6'>
         <h2 className='text-2xl font-bold mb-4'>About Me</h2>
-        <p className='text-gray-400'>
-            I am an Ict Management in Norton University, intersting with Web Development espacailly with Front-end
+
+        <p className='text-gray-400 text-lg'>
+          I am an ICT Management student at Norton University,
+          <span className='text-blue-500 font-semibold ml-2'>
+            <Typewriter
+              words={[
+                'interested in Web Development',
+                'Front-End Developer',
+                'React JS Developer',
+                'Tailwind CSS'
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="|"
+              typeSpeed={70}
+              deleteSpeed={50}
+              delaySpeed={1500}
+            />
+          </span>
         </p>
+
       </section>
     </div>
   )
